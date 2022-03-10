34e5b36643e816f983e8523f09b5b81da1dda958
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-10 14:20:04 -0800

    write out first test for Galactic object

---
+++ __tests__/galactic.test.js
@@ -0,0 +1,8 @@
+import Galactic from './../src/js/character.js'
+
+describe('Galactic', () => {
+
+  test('should return the object galactic', () => {
+    const galactic = new Galactic(;)
+  })
+})
