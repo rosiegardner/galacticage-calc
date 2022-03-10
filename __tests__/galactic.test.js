cf183fdd6cb83defaea8fd02c4a975d2cad09e35
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-10 14:57:49 -0800

    write second test

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,7 +2,12 @@ import Galactic from './../src/js/galactic.js'
 
 describe('Galactic', () => {
 
-  test('should return the object galactic', () => {
-    const galactic = new Galactic();
+  test('should return the class galactic', () => {
+    const galacticAge = new Galactic ();
+  });
+
+  test('should return the constructor of galactic class', () => {
+    const galacticAge = new Galactic ();
+    expect(galacticAge.age).toEqual();
   });
 });
