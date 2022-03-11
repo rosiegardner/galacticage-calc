ffbfa2d1a6863c8c61a5140e92d511b40163d3e0
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-10 21:47:10 -0800

    update src folder - galactic.js file

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -1,17 +1,19 @@
 export default class Galactic {
-  constructor(earth) {
-    this.earth = earth;
-    // this.mercury = this.earth * .24;
+  constructor(earthSolar) {
+    this.earthSolar = earthSolar;
+    this.mercury = this.earth * .24;
     // this.venus = this.earth * .62;
     // this.mars = this.earth * 1.88;
     // this.jupiter = this.earth * 11.86;
     
   }
 
-  // lifeExpectancy() {
-  //  this.earthLife = 69; //Life expectancy of earthling
-  //  this.earthDeath = this.earthLife - this.earth; // Life left remaining of earthling
-  // }
+  lifeExpectancy() {
+   this.earthLife = 69; //Life expectancy of earthling
+   this.earthDeath = this.earthLife - this.earthSolar; // Life left remaining of earthling
+
+   this.bonusEarthYears = Math.abs(this.earthDeath); 
+  }
 
   
 }
