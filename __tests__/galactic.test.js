27915337e764e705cc7ebc1c1eda72b5a8db6200
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-10 20:02:59 -0800

    WIP: refactor and remove all testing and BILogic from js

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,39 +2,8 @@ import Galactic from './../src/js/galactic.js'
 
 describe('Galactic', () => {
 
-  test('should return the class galactic', () => {
-    const galactic = new Galactic();
-  });
-
-  test('should return the constructor of galactic class', () => {
-    const galactic = new Galactic();
-    expect(galactic.galacticAge).toEqual();
-  });
-
-  test('should return the age of user', () => {
-    const galactic = new Galactic(32);
-    expect(galactic.galacticAge).toEqual(32);
-  });
-
-  test('should return the age of user life expectancy', () => {
-    const galactic = new Galactic(32);
-    expect(galactic.galacticAge).toEqual(32);
-    expect(galactic.lifeExpectancy).toEqual(100);
-  });
-
-});
-
-describe('planetMercury', () => {
-
-  test('should create the function planetMercury', () => {
-    const galactic = new Galactic(32);
-    galactic.planetMercury();
-  });
-
-  test('should return the age on planetMercury', () => {
-    const galactic = new Galactic(32);
-    let mercuryAge = galactic.planetMercury();
-    expect(mercuryAge).toEqual(133)
-  });
+  // test('should return the class galactic', () => {
+  //   const galactic = new Galactic();
+  // });
 
 });
