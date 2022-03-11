9a1336028b9776c2e4261dcf0377fd61a0fe3a31
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-10 20:51:29 -0800

    wrote 3rd test for lifeExpectancy

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -23,4 +23,10 @@ describe('LifeExpectancy', () => {
     expect(lifeRemains.earthDeath).toEqual(1)
   });
 
+  test('should return years surpass life expectancy', () => {
+    const lifeSurpass = new Galactic(70);
+    lifeSurpass.lifeExpectancy();
+    expect(lifeSurpass.bonusEarthYears).toEqual(-1)
+  });
+
 });
