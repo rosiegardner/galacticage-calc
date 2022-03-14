d690aa7cf6d9931682bf7601a71a3b67db63bef8
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-14 16:21:53 -0700

    refactor 1st test for lifeExpectancy

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -47,27 +47,28 @@ describe('Galactic', () => {
     
   });
 
+  describe('LifeExpectancy', () => {
+    
+    test('should return the expectancy of life on earth', () => {
+      const lifeLimit = new Galactic();
+      lifeLimit.lifeExpectancy();
+      expect(lifeLimit.expectancy).toEqual(69);
+    });
+  
+    // test('should return the the remaining years of life', () => {
+    //   const lifeRemains = new Galactic(68);
+    //   lifeRemains.lifeExpectancy();
+    //   expect(lifeRemains.earthDeath).toEqual(1)
+    // });
+  
+    // test('should return years surpass life expectancy', () => {
+    //   const lifeSurpass = new Galactic(70);
+    //   lifeSurpass.lifeExpectancy();
+    //   expect(lifeSurpass.bonusEarthYears).toEqual(1)
+    // });
+  
+  });
+
 });
 
-// describe('LifeExpectancy', () => {
-  
-//   test('should return the expectancy of life on earth', () => {
-//     const lifeLimit = new Galactic();
-//     lifeLimit.lifeExpectancy();
-//     expect(lifeLimit.earthLife).toEqual(69);
-//   });
-
-//   test('should return the the remaining years of life', () => {
-//     const lifeRemains = new Galactic(68);
-//     lifeRemains.lifeExpectancy();
-//     expect(lifeRemains.earthDeath).toEqual(1)
-//   });
-
-//   test('should return years surpass life expectancy', () => {
-//     const lifeSurpass = new Galactic(70);
-//     lifeSurpass.lifeExpectancy();
-//     expect(lifeSurpass.bonusEarthYears).toEqual(1)
-//   });
-
-// });
 
