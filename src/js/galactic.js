a5450f7546c0c529c3c5e2571ce357b77a1708c2
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-15 17:16:58 -0700

    create galatic.js with corresponding code

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -1,19 +1,44 @@
 export default class Galactic {
-  constructor(earthSolar) {
-    this.earthSolar = earthSolar;
-    this.mercury = this.earth * .24;
-    // this.venus = this.earth * .62;
-    // this.mars = this.earth * 1.88;
-    // this.jupiter = this.earth * 11.86;
-    
+  constructor(age) {
+    this.age = age; 
+    this.mercurySolar = 0;
+    this.venusSolar = 0;
+    this.marsSolar = 0;
+    this.jupiterSolar = 0;
+    this.expectancy = 69;
+    this.ageOver = 0;
   }
 
-  lifeExpectancy() {
-   this.earthLife = 69; //Life expectancy of earthling
-   this.earthDeath = this.earthLife - this.earthSolar; // Life left remaining of earthling
-
-   this.bonusEarthYears = Math.abs(this.earthDeath); 
+  lifeExpectancy() { 
+   this.expectancy = (this.expectancy - this.age);
+   this.ageOver = Math.abs(this.expectancy);
   }
+
+  mercuryAge() {
+    this.mercurySolar = (this.age * .24);
+    this.mercuryExpectancy = (this.expectancy * .24);
+    this.mercuryAgeOver = (this.ageOver * .24);
+  }
+
+  venusAge() {
+    this.venusSolar = (this.age * .62);
+    this.venusExpectancy = (this.expectancy * .62);
+    this.venusAgeOver = (this.ageOver * .62);
+  }
+
+  marsAge() {
+    this.marsSolar = (this.age * 1.88);
+    this.marsExpectancy = (this.expectancy * 1.88);
+    this.marsAgeOver = (this.ageOver * 1.88);
+  }
+
+  jupiterAge() {
+    this.jupiterSolar = (this.age * 11.86);
+    this.jupiterExpectancy = (this.expectancy * 11.86);
+    this.jupiterAgeOver = (this.ageOver * 11.86);
+  }
+
+  
 
   
 }
